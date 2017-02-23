afc9333a607797c2f94a9d30db6077c686facb07
aturtur <rautio.arttu@gmail.com>
2017-02-23 21:52:35 +0200

    Fixing horoscope fuction

--- libbot.py
+++ libbot.py
@@ -122,21 +122,21 @@ class Bot(object):
         Args:
             horoscope (string):
         """
-
-        print horoscope
-
         horoscopes = ['oinas','härkä','kaksonen','rapu','leijona','neitsyt','vaaka','skorpioni','jousimies','kauris','vesimies','kalat']
         horoscope = horoscope.lower()
         if horoscope in horoscopes:
             horoscope_url = 'http://www.iltalehti.fi/horoskooppi/'
             horoscope_request = urllib2.Request(horoscope_url)
             horoscope_response = urllib2.urlopen(horoscope_request)
-            content = horoscope_response.read()
-            content = content.lower()
+            content = horoscope_response.read().lower()
             find = '<p>{}'.format(horoscope)
             splitdata = content.split(find, 1)
             answer = splitdata[1].split('</p>', 1)
-            self.chat(horoscope + answer[0])
+            result = ""    
+            sentence = answer[0].split(". ")
+            for x in sentence:
+                result += x[0].upper() + x[1:] + '. '
+            self.chat(horoscope.capitalize() + result[:-2])
         else:
             self.chat('En ymmärtänyt')
 
